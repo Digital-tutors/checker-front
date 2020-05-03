24fbf532f2e52e3c3e559ed77c176b936937df6d
d3byte <pandinator@protonmail.com>
2020-05-03 19:30:12 +0300

    feat: empty task page for user

---
+++ src/app/user/pages/task/task.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { TaskComponent } from './task.component';
+
+@NgModule({
+  declarations: [TaskComponent],
+  imports: [CommonModule],
+})
+export class TaskModule {}
