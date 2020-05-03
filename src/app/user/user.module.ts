77ed6a7bd165e430c80d49ddba7235a2fa420524
d3byte <pandinator@protonmail.com>
2020-05-03 19:44:02 +0300

    feat: advanced routing for user

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 
 import { ShareModule } from '@share/share.module';
 
+import { LayoutComponent } from './components/layout/layout.component';
+import { WrapperComponent } from './components/wrapper/wrapper.component';
 import { RoutingModule } from './user.routes';
 
 @NgModule({
-  declarations: [],
+  declarations: [LayoutComponent, WrapperComponent],
   imports: [ShareModule, RoutingModule],
 })
 export class UserModule {}
--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -1,20 +1,28 @@
 import { ModuleWithProviders } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { WrapperComponent } from './components/wrapper/wrapper.component';
+
 const routes: Routes = [
   {
-    path: 'topics',
-    pathMatch: 'full',
-    loadChildren: () => import('./pages/topics/topics.module').then(m => m.TopicsModule),
-  },
-  {
-    path: 'topics/:id/tasks',
-    pathMatch: 'full',
-    loadChildren: () => import('./pages/tasks/tasks.module').then(m => m.TasksModule),
-  },
-  {
-    path: 'topics/:id/tasks/:taskId',
-    loadChildren: () => import('./pages/task/task.module').then(m => m.TaskModule),
+    path: '',
+    component: WrapperComponent,
+    children: [
+      {
+        path: 'topics',
+        pathMatch: 'full',
+        loadChildren: () => import('./pages/topics/topics.module').then(m => m.TopicsModule),
+      },
+      {
+        path: 'topics/:id/tasks',
+        pathMatch: 'full',
+        loadChildren: () => import('./pages/tasks/tasks.module').then(m => m.TasksModule),
+      },
+      {
+        path: 'topics/:id/tasks/:taskId',
+        loadChildren: () => import('./pages/task/task.module').then(m => m.TaskModule),
+      },
+    ],
   },
 ];
 
