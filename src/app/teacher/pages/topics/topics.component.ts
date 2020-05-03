277540f1fbde48e0545776b2dd7504ed60d1df3c
d3byte <pandinator@protonmail.com>
2020-05-03 19:45:14 +0300

    feat: topics page for user

---
+++ src/app/teacher/pages/topics/topics.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher-topics',
+  templateUrl: './topics.component.html',
+  styleUrls: ['./topics.component.scss'],
+})
+export class TopicsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/teacher/pages/topics/topics.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { TopicsComponent } from './topics.component';
+
+@NgModule({
+  declarations: [TopicsComponent],
+  imports: [CommonModule],
+})
+export class TopicsModule {}
