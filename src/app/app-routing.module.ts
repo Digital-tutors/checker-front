e9a3ac7b43ac019d6b6e0b611f58a6f7e58bd10f
d3byte <pandinator@protonmail.com>
2020-05-03 19:45:39 +0300

    feat: app routing with lazy loadable modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from '@share/guards/auth.guard';
+import { UnAuthGuard } from '@share/guards/un-auth.guard';
+
 const routes: Routes = [
   {
     path: 'auth',
+    canActivate: [UnAuthGuard],
     loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+  },
+  {
+    path: 'user',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./user/user.module').then(m => m.UserModule),
+  },
+  {
+    path: 'admin',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
+  },
+  {
+    path: 'teacher',
+    canActivate: [AuthGuard],
+    loadChildren: () => import('./teacher/teacher.module').then(m => m.TeacherModule),
   },
 ];
 
