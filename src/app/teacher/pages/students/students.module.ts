f36ba421c8c89ba8283cfc70e5ce738b0b98ff18
d3byte <pandinator@protonmail.com>
2020-05-03 19:45:04 +0300

    feat: students page for teacher

---
+++ src/app/teacher/pages/students/students.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { StudentsComponent } from './students.component';
+
+@NgModule({
+  declarations: [StudentsComponent],
+  imports: [CommonModule],
+})
+export class StudentsModule {}
