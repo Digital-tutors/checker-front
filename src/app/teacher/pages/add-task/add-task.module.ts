7b17be31e5c71405f4a74d9b062e79534a1fb6f0
d3byte <pandinator@protonmail.com>
2020-05-03 19:44:53 +0300

    feat: add task page for teacher

---
+++ src/app/teacher/pages/add-task/add-task.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { AddTaskComponent } from './add-task.component';
+
+@NgModule({
+  declarations: [AddTaskComponent],
+  imports: [CommonModule],
+})
+export class AddTaskModule {}
