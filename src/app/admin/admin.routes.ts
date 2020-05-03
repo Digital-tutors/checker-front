1d6ecf6222ee2f5e387b13824191663d9416a0aa
d3byte <pandinator@protonmail.com>
2020-05-03 19:28:56 +0300

    feat: empty module for admin

---
+++ src/app/admin/admin.routes.ts
@@ -0,0 +1,6 @@
+import { ModuleWithProviders } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+export const RoutingModule: ModuleWithProviders = RouterModule.forChild(routes);
