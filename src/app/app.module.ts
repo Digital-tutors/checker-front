35c8466279e50875b2b1f9df7012ee3e110d0bbb
Daniil Vilyavin <89505105005@mail.ru>
2020-05-07 18:02:12 +0300

    Add material design and create header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { NgxsModule } from '@ngxs/store';
 
@@ -33,6 +34,7 @@ export function GetApiConfiguration() {
       developmentMode: !environment.production,
     }),
     ApiModule.forRoot(GetApiConfiguration),
+    BrowserAnimationsModule,
   ],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }],
   bootstrap: [AppComponent],
--- src/app/user/pages/task/task.module.ts
+++ src/app/user/pages/task/task.module.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { HeaderComponent } from '../../components/header/header.component';
+
 import { TaskComponent } from './task.component';
 import { RoutingModule } from './task.routes';
 
 @NgModule({
-  declarations: [TaskComponent],
-  imports: [CommonModule, RoutingModule],
+  declarations: [TaskComponent, HeaderComponent],
+  imports: [CommonModule, RoutingModule, MatToolbarModule, MatIconModule, MatButtonModule],
 })
 export class TaskModule {}
