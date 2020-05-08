38cb8bc0698ef84fbcdacbbb1e558f7e94c450fc
Daniil Vilyavin <89505105005@mail.ru>
2020-05-08 23:10:30 +0300

    Получение таски с бека и отправка своего решения обратно

--- src/app/user/pages/task/task.component.ts
+++ src/app/user/pages/task/task.component.ts
@@ -6,6 +6,8 @@ import { Subject } from 'rxjs';
 import { flatMap, takeUntil, tap } from 'rxjs/operators';
 
 import { TaskControllerService } from '@swagger/api/taskController.service';
+import { TaskResultsControllerService } from '@swagger/api/taskResultsController.service';
+import { TaskResultsCreateRq } from '@swagger/model/taskResultsCreateRq';
 import { TaskVO } from '@swagger/model/taskVO';
 
 @Component({
@@ -16,14 +18,21 @@ import { TaskVO } from '@swagger/model/taskVO';
 export class TaskComponent implements OnInit, OnDestroy {
   private ngOnDestroy$: Subject<void> = new Subject();
   private taskId: string;
-  private task: TaskVO;
+  topicId: string;
+  task: TaskVO;
+  spinner = false;
 
   public editorOptions = { theme: 'vs-dark', language: 'cpp' };
   public code = '/*\n\tЭто мини-редактор VS Code.\n\tПисать код вы можете ниже или вместо комментария\n*/\n\n';
 
   public codeForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private taskControllerService: TaskControllerService, private route: ActivatedRoute) {}
+  constructor(
+    private fb: FormBuilder,
+    private taskControllerService: TaskControllerService,
+    private taskResultsControllerService: TaskResultsControllerService,
+    private route: ActivatedRoute,
+  ) {}
 
   ngOnInit(): void {
     this.setForm();
@@ -32,6 +41,7 @@ export class TaskComponent implements OnInit, OnDestroy {
       .pipe(
         tap(params => {
           this.taskId = params.get('taskId');
+          this.topicId = params.get('id');
         }),
         flatMap(params => this.taskControllerService.getTaskByIdUsingGET(params.get('taskId'))),
         takeUntil(this.ngOnDestroy$),
@@ -54,7 +64,22 @@ export class TaskComponent implements OnInit, OnDestroy {
   }
 
   public sendCode(): void {
-    console.log(this.getHandledCode());
+    this.spinner = true;
+
+    const taskResults: TaskResultsCreateRq = {
+      taskId: {
+        id: this.taskId,
+      },
+      userId: {
+        id: '5eb45214ea2ee10742104e1f',
+      },
+      language: this.editorOptions.language,
+      sourceCode: this.getHandledCode(),
+    };
+
+    this.taskResultsControllerService.saveDecisionUsingPOST(taskResults).subscribe(_ => {
+      this.spinner = false;
+    });
   }
 
   public selectLanguage(language: string): void {
--- src/app/user/pages/task/task.module.ts
+++ src/app/user/pages/task/task.module.ts
@@ -6,6 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatOptionModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -27,6 +28,7 @@ import { MonacoEditorModule } from 'ngx-monaco-editor';
     ReactiveFormsModule,
     MatOptionModule,
     MatSelectModule,
+    MatProgressSpinnerModule,
   ],
 })
 export class TaskModule {}
