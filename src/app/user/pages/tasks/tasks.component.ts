1414d234f379954de08127b53e636823b7c87d1f
Adlls <adlansaidullaev@gmail.com>
2020-05-08 23:24:07 +0300

    some change for tasks component

--- src/app/user/pages/tasks/tasks.component.ts
+++ src/app/user/pages/tasks/tasks.component.ts
@@ -9,12 +9,34 @@ export interface task {
   isComplete: boolean;
 }
 
+// tslint:disable-next-line:class-name
+export interface topic {
+  id?: number;
+  title: string;
+  author: string;
+  access: string;
+  description: string;
+  isFollow: boolean;
+}
+
 @Component({
   selector: 'app-user-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.scss'],
 })
 export class TasksComponent implements OnInit {
+  topic: topic = {
+    title: 'title topic',
+    author: 'Ivanov',
+    access: 'Пулбичный',
+    description:
+      'Lorem ipsum dolor sit amet, ' +
+      'consectetur adipisicing elit. Asperiores at ' +
+      'dolorum eos error fugiat ipsam laboriosam minus ' +
+      'mollitia quo voluptatem! Alias atque commodi facere illum iste magni nam quisquam similique?',
+    isFollow: true,
+  };
+
   tasks: task[] = [
     {
       title: 'task1',
