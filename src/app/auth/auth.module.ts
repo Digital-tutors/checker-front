d6231a87f29926f25dc33e6ab89627aa19dfb1f8
Daniil Vilyavin <89505105005@mail.ru>
2020-05-09 22:10:47 +0300

    Login page

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 import { ShareModule } from '@share/share.module';
 
 import { LoginComponent } from './pages/login/login.component';
@@ -8,6 +13,6 @@ import { RoutingModule } from './auth.routes';
 
 @NgModule({
   declarations: [LoginComponent, SignupComponent],
-  imports: [ShareModule, RoutingModule],
+  imports: [ShareModule, RoutingModule, MatCardModule, MatFormFieldModule, MatInputModule, MatButtonModule],
 })
 export class AuthModule {}
