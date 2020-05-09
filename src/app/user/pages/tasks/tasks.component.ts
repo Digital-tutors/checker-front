e8953d86b448fed61b1c8b917ed3ae091b9e80f8
Daniil Vilyavin <89505105005@mail.ru>
2020-05-09 16:25:31 +0300

    Enum class for subscribing

--- src/app/user/pages/tasks/tasks.component.ts
+++ src/app/user/pages/tasks/tasks.component.ts
@@ -9,6 +9,12 @@ import { TopicControllerService } from '@swagger/api/topicController.service';
 import { TaskVO } from '@swagger/model/taskVO';
 import { TopicVO } from '@swagger/model/topicVO';
 
+enum SubscribeStatus {
+  NOT_SUBSCRIBED,
+  LOAD,
+  SUBSCRIBED,
+}
+
 @Component({
   selector: 'app-user-tasks',
   templateUrl: './tasks.component.html',
@@ -19,7 +25,7 @@ export class TasksComponent implements OnInit, OnDestroy {
   topicId: string;
   topic: TopicVO;
   tasks: TaskVO[];
-  spinner = 0;
+  spinner = SubscribeStatus.NOT_SUBSCRIBED;
 
   constructor(private taskControllerService: TaskControllerService, private topicControllerService: TopicControllerService, private route: ActivatedRoute) {}
 
@@ -34,7 +40,13 @@ export class TasksComponent implements OnInit, OnDestroy {
         flatMap(_ => this.topicControllerService.getTopicByIdUsingGET(this.topicId)),
         takeUntil(this.ngOnDestroy$),
       )
-      .subscribe(topic => (this.topic = topic));
+      .subscribe(topic => {
+        this.topic = topic;
+        this.spinner =
+          this.topic.followers.filter(value => value.id === '5eb45214ea2ee10742104e1f').length > 0
+            ? SubscribeStatus.SUBSCRIBED
+            : SubscribeStatus.NOT_SUBSCRIBED;
+      });
   }
 
   ngOnDestroy(): void {
@@ -42,10 +54,10 @@ export class TasksComponent implements OnInit, OnDestroy {
   }
 
   subscribe() {
-    this.spinner = 1;
+    this.spinner = SubscribeStatus.LOAD;
 
     this.topicControllerService.subscribeTopicUsingPOST(this.topicId, '5eb45214ea2ee10742104e1f').subscribe(_ => {
-      this.spinner = 2;
+      this.spinner = SubscribeStatus.SUBSCRIBED;
     });
   }
 }
