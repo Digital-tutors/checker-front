506b2f4b37f68420fe1dc8992facb2c14a00958e
Daniil Vilyavin <89505105005@mail.ru>
2020-06-19 19:48:58 +0300

    Create form for adding new topic

--- src/app/teacher/pages/topics/topics.module.ts
+++ src/app/teacher/pages/topics/topics.module.ts
@@ -3,7 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 import { AddTopicComponent } from './add/add.component';
 import { TopicsComponent } from './topics.component';
@@ -11,6 +14,6 @@ import { RoutingModule } from './topics.routes';
 
 @NgModule({
   declarations: [TopicsComponent, AddTopicComponent],
-  imports: [CommonModule, RoutingModule, MatCardModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, RoutingModule, MatCardModule, MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatSelectModule],
 })
 export class TopicsModule {}
