866b1380b679d8534d1a1d742db92111f8b2ff2b
Daniil Vilyavin <89505105005@mail.ru>
2020-06-20 20:21:22 +0300

    Complete add new task with backend

--- src/app/teacher/pages/topic/add-task/add.component.ts
+++ src/app/teacher/pages/topic/add-task/add.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { EMPTY, from, Subject } from 'rxjs';
 import { catchError, flatMap, takeUntil, tap } from 'rxjs/operators';
 
 import { TopicControllerService } from '@swagger/api/topicController.service';
+import ScrollbarVisibility = monaco.editor.ScrollbarVisibility;
+import { TaskControllerService } from '@swagger/api/taskController.service';
+import { TaskCreateRq } from '@swagger/model/taskCreateRq';
 
 @Component({
   selector: 'app-add-task',
@@ -17,14 +20,29 @@ export class AddTaskComponent implements OnInit, OnDestroy {
 
   public form: FormGroup;
 
+  public topicId: string;
+
   public error: boolean;
 
-  public editorOptions = { theme: 'vs-dark', language: 'cpp' };
+  public editorOptions = {
+    minimap: {
+      enabled: false,
+    },
+    language: 'cpp',
+  };
 
-  constructor(private fb: FormBuilder, private topicControllerService: TopicControllerService, private router: Router) {}
+  constructor(private fb: FormBuilder, private taskControllerService: TaskControllerService, private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
     this.setForm();
+
+    this.route.paramMap
+      .pipe(
+        tap(params => {
+          this.topicId = params.get('id');
+        }),
+      )
+      .subscribe();
   }
 
   private setForm(): void {
@@ -33,8 +51,8 @@ export class AddTaskComponent implements OnInit, OnDestroy {
       level: ['', [Validators.required]],
       description: ['', [Validators.required]],
       timeLimit: ['', [Validators.required, Validators.max(8)]],
-      memoryLimit: ['', [Validators.required, Validators.max(5)]],
-      constructions: ['', [Validators.required]],
+      memoryLimit: ['', [Validators.required, Validators.max(256)]],
+      constructions: ['', [Validators.pattern('^[a-zA-Z,]*')]],
       codes: this.fb.array([this.createItem(), this.createItem(), this.createItem()]),
     });
   }
@@ -49,8 +67,8 @@ export class AddTaskComponent implements OnInit, OnDestroy {
 
   public createItem(): FormGroup {
     return this.fb.group({
-      input: '',
-      output: '',
+      input: ['', [Validators.required]],
+      output: ['', [Validators.required]],
     });
   }
 
@@ -58,7 +76,44 @@ export class AddTaskComponent implements OnInit, OnDestroy {
     this.codes.push(this.createItem());
   }
 
+  public removeItem(): void {
+    if (this.codes.length > 3) {
+      this.codes.removeAt(this.codes.length - 1);
+    }
+  }
+
   public onSubmit(): void {
-    console.log(this.form.value);
+    const { title, level, description, timeLimit, memoryLimit, constructions, codes } = this.form.value;
+
+    const createTask: TaskCreateRq = {
+      title,
+      topicId: { id: this.topicId },
+      level,
+      description,
+      options: {
+        constructions: constructions.toString().replace(' ', '').split(','),
+        timeLimit,
+        memoryLimit,
+      },
+      tests: {
+        input: [...codes.map(v => v.input)],
+        output: [...codes.map(v => v.output)],
+      },
+    };
+
+    if (this.form.valid) {
+      this.taskControllerService
+        .createTaskUsingPOST(createTask)
+        .pipe(
+          catchError(() => {
+            this.error = true;
+            return EMPTY;
+          }),
+          takeUntil(this.ngOnDestroy$),
+        )
+        .subscribe(() => {
+          from(this.router.navigateByUrl(`/teacher/topic/${this.topicId}`));
+        });
+    }
   }
 }
