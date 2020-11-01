6115f78f8f36f2fe3863b2256c398cf7ed1e24c7
d3byte <pandinator@protonmail.com>
2020-11-01 19:23:23 +0300

    feat: updated share module

--- src/app/share/share.module.ts
+++ src/app/share/share.module.ts
@@ -4,10 +4,23 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MatIconModule } from '@angular/material/icon';
 
-import { ImageCardComponent } from '@share/components/image-card/image-card.component';
-import { InfoContainerComponent } from '@share/components/info-container/info-container.component';
+import { ButtonComponent } from './components/button/button.component';
+import { HighlightedContainerComponent } from './components/highlighted-container/highlighted-container.component';
+import { ImageCardComponent } from './components/image-card/image-card.component';
+import { InfoContainerComponent } from './components/info-container/info-container.component';
+import { RatingComponent } from './components/rating/rating.component';
+import { ShadowContainerComponent } from './components/shadow-container/shadow-container.component';
+import { TwoBoxGridComponent } from './components/two-box-grid/two-box-grid.component';
 
-const COMPONENTS: any[] = [ImageCardComponent, InfoContainerComponent];
+const COMPONENTS: any[] = [
+  ImageCardComponent,
+  InfoContainerComponent,
+  TwoBoxGridComponent,
+  RatingComponent,
+  ButtonComponent,
+  ShadowContainerComponent,
+  HighlightedContainerComponent,
+];
 
 const MATERIAL_MODULES: any[] = [MatIconModule];
 
