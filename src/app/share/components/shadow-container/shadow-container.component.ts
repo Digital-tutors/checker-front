48c2bbb61943249f3e1863a8a3d333ae7c04ff33
d3byte <pandinator@protonmail.com>
2020-11-01 19:22:47 +0300

    feat: shadow container

---
+++ src/app/share/components/shadow-container/shadow-container.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shadow-container',
+  templateUrl: './shadow-container.component.html',
+  styleUrls: ['./shadow-container.component.scss'],
+})
+export class ShadowContainerComponent {}
