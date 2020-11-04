de140f1a4d05b34838e112e3cfe46a55f88d5520
Valoriel <kostyan001301@gmail.com>
2020-11-05 01:58:30 +0300

    feat: Добавление занятия

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -13,6 +14,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { ShareModule } from '@share/share.module';
 
 import { AboutCourseSidebarComponent } from './components/about-course-sidebar/about-course-sidebar.component';
+import { AlertWindowComponent } from './components/alert-window/alert-window.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { LessonPageSidebarComponent } from './components/lesson-page-sidebar/lesson-page-sidebar.component';
 import { AdminComponent } from './components/quiz/admin.component';
@@ -21,7 +23,15 @@ import { WrapperComponent } from './components/wrapper/wrapper.component';
 import { RoutingModule } from './admin.routes';
 
 @NgModule({
-  declarations: [LayoutComponent, WrapperComponent, AboutCourseSidebarComponent, TopicSidebarComponent, AdminComponent, LessonPageSidebarComponent],
+  declarations: [
+    LayoutComponent,
+    WrapperComponent,
+    AboutCourseSidebarComponent,
+    TopicSidebarComponent,
+    AdminComponent,
+    LessonPageSidebarComponent,
+    AlertWindowComponent,
+  ],
   imports: [
     ShareModule,
     RoutingModule,
@@ -34,7 +44,9 @@ import { RoutingModule } from './admin.routes';
     MatSelectModule,
     MatSlideToggleModule,
     MatDividerModule,
+    MatDialogModule,
   ],
   exports: [],
+  entryComponents: [AlertWindowComponent],
 })
 export class AdminModule {}
--- src/app/admin/pages/lesson-page/lesson-page.component.ts
+++ src/app/admin/pages/lesson-page/lesson-page.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MatDialog } from '@angular/material/dialog';
+
 import { SidebarService } from '@share/services/sidebar.service';
 
 import { LessonPageSidebarComponent } from '../../components/lesson-page-sidebar/lesson-page-sidebar.component';
+
+import { AlertWindowComponent } from 'app/admin/components/alert-window/alert-window.component';
 
 @Component({
   selector: 'app-lesson-page',
@@ -10,9 +14,16 @@ import { LessonPageSidebarComponent } from '../../components/lesson-page-sidebar
   styleUrls: ['./lesson-page.component.scss'],
 })
 export class LessonPageComponent implements OnInit {
-  constructor(private sidebarService: SidebarService) {}
+  constructor(private sidebarService: SidebarService, private dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.sidebarService.setSidebar(LessonPageSidebarComponent);
+  }
+
+  public openDialog(): void {
+    const dialogRef = this.dialog.open(AlertWindowComponent, {
+      width: '473px',
+      height: '270px',
+    });
   }
 }
