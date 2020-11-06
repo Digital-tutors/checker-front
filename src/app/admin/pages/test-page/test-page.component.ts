7415f8a6ad6ce82870257d92cff7396601f0f444
Valoriel <kostyan001301@gmail.com>
2020-11-07 02:09:27 +0300

    wip: test-page

--- src/app/admin/pages/test-page/test-page.component.ts
+++ src/app/admin/pages/test-page/test-page.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+import { MatDialog } from '@angular/material/dialog';
+
+import { SidebarService } from '@share/services/sidebar.service';
+
+import { AlertWindowTestComponent } from 'app/admin/components/alert-window-test/alert-window-test.component';
+import { TestPageSidebarComponent } from 'app/admin/components/test-page-sidebar/test-page-sidebar.component';
 
 @Component({
   selector: 'app-test-page',
@@ -6,7 +14,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test-page.component.scss'],
 })
 export class TestPageComponent implements OnInit {
-  constructor() {}
+  public form: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(private sidebarService: SidebarService, private dialog: MatDialog, private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.sidebarService.setSidebar(TestPageSidebarComponent);
+    this.setForm();
+  }
+
+  public openDialog(): void {
+    const dialogRef = this.dialog.open(AlertWindowTestComponent, {
+      width: '473px',
+      height: '224px',
+    });
+  }
+
+  private setForm(): void {
+    this.form = this.fb.group({
+      title: [''],
+      level: [''],
+      isLevel1Enabled: [''],
+      isLevel2Enabled: [''],
+      isLevel3Enabled: [''],
+    });
+  }
 }
--- src/app/admin/pages/test-page/test-page.module.ts
+++ src/app/admin/pages/test-page/test-page.module.ts
@@ -1,10 +1,31 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+
+import { ShareModule } from '@share/share.module';
+
 import { TestPageComponent } from './test-page.component';
+import { RoutingModule } from './test-page.routes';
 
 @NgModule({
   declarations: [TestPageComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RoutingModule,
+    ShareModule,
+    RoutingModule,
+    MatExpansionModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+  ],
 })
 export class TestPageModule {}
