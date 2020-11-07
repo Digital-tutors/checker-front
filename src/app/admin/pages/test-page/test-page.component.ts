4d0a286f4d14ba2ddfc68751d8de8a3a6b8387a5
Valoriel <kostyan001301@gmail.com>
2020-11-07 16:18:02 +0300

    wip: test-page

--- src/app/admin/pages/test-page/test-page.component.ts
+++ src/app/admin/pages/test-page/test-page.component.ts
@@ -1,12 +1,18 @@
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
+import { MatChipInputEvent } from '@angular/material/chips';
 import { MatDialog } from '@angular/material/dialog';
 
 import { SidebarService } from '@share/services/sidebar.service';
 
 import { AlertWindowTestComponent } from 'app/admin/components/alert-window-test/alert-window-test.component';
 import { TestPageSidebarComponent } from 'app/admin/components/test-page-sidebar/test-page-sidebar.component';
+
+export interface Fruit {
+  name: string;
+}
 
 @Component({
   selector: 'app-test-page',
@@ -15,6 +21,13 @@ import { TestPageSidebarComponent } from 'app/admin/components/test-page-sidebar
 })
 export class TestPageComponent implements OnInit {
   public form: FormGroup;
+
+  visible = true;
+  selectable = true;
+  removable = true;
+  addOnBlur = true;
+  readonly separatorKeysCodes: number[] = [ENTER, COMMA];
+  fruits: Fruit[] = [];
 
   constructor(private sidebarService: SidebarService, private dialog: MatDialog, private fb: FormBuilder) {}
 
@@ -38,5 +51,28 @@ export class TestPageComponent implements OnInit {
       isLevel2Enabled: [''],
       isLevel3Enabled: [''],
     });
+  }
+
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+
+    // Add our fruit
+    if ((value || '').trim()) {
+      this.fruits.push({ name: value.trim() });
+    }
+
+    // Reset the input value
+    if (input) {
+      input.value = '';
+    }
+  }
+
+  remove(fruit: Fruit): void {
+    const index = this.fruits.indexOf(fruit);
+
+    if (index >= 0) {
+      this.fruits.splice(index, 1);
+    }
   }
 }
--- src/app/admin/pages/test-page/test-page.module.ts
+++ src/app/admin/pages/test-page/test-page.module.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -26,6 +28,8 @@ import { RoutingModule } from './test-page.routes';
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
+    MatChipsModule,
+    MatAutocompleteModule,
   ],
 })
 export class TestPageModule {}
